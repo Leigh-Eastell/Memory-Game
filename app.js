91a65c928eaeaddbb1b8de48c36d1e7819a5061a
leigheastell <leigheastell1286@gmail.com>
2022-09-26 09:51:17 +0100

    created game board and card array

---
+++ app.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //card options
+  const cardArray = [
+    {
+      name: "captain price",
+      img: "images/captain price.jpg",
+    },
+    {
+      name: "captain price",
+      img: "images/captain price.jpg",
+    },
+    {
+      name: "fortnite",
+      img: "images/fortnite.jpg",
+    },
+    {
+      name: "fortnite",
+      img: "images/fortnite.jpg",
+    },
+    {
+      name: "GOW",
+      img: "images/GOW.jpg",
+    },
+    {
+      name: "GOW",
+      img: "images/GOW.jpg",
+    },
+    {
+      name: "mastechief",
+      img: "images/masterchief.jpg",
+    },
+    {
+      name: "mastechief",
+      img: "images/masterchief.jpg",
+    },
+    {
+      name: "minecraft",
+      img: "images/minecraft",
+    },
+    {
+      name: "minecraft",
+      img: "images/minecraft",
+    },
+    {
+      name: "rocket league",
+      img: "images/rocket league.jpg",
+    },
+    {
+      name: "rocket league",
+      img: "images/rocket league.jpg",
+    },
+  ];
+
+  const grid = document.querySelector(".grid");
+
+  //create board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      var card = document.createElement("img");
+      card.setAttribute("src", "images/blue.png");
+      card.setAttribute("data-id", i);
+      // card.addEventListener('click', flipcard)
+      grid.appendChild(card);
+    }
+  }
+
+  createBoard();
+});
